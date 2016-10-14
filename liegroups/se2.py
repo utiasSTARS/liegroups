Updated tests and added SO2/3 Jacobians so that SE2/3 exp is actually correct

liegroups/se2.py
@@ -23,19 +23,19 @@ class SE2:
         self.trans = trans
 
     @classmethod
-    def frommatrix(cls, mat):
+    def from_matrix(cls, mat):
         """Create a SE2 object from a 3x3 transformation matrix."""
-        if not SE2.isvalidmatrix(mat):
+        if not SE2.is_valid_matrix(mat):
             raise ValueError("Invalid transformation matrix")
 
         return cls(SO2(mat[0:2, 0:2]), mat[0:2, 2])
 
     @classmethod
-    def isvalidmatrix(cls, mat):
+    def is_valid_matrix(cls, mat):
         """Check if a matrix is a valid transformation matrix."""
         return mat.shape == (3, 3) and \
             np.array_equal(mat[2, :], np.array([0, 0, 1])) and \
-            SO2.isvalidmatrix(mat[0:2, 0:2])
+            SO2.is_valid_matrix(mat[0:2, 0:2])
 
     @classmethod
     def identity(cls):
@@ -84,7 +84,9 @@ class SE2:
         if xi.size != 3:
             raise ValueError("xi must have size 3")
 
-        return cls(SO2.exp(xi[2]), xi[0:2])
+        rho = xi[0:2]
+        phi = xi[2]
+        return cls(SO2.exp(phi), SO2.left_jacobian(phi).dot(rho))
 
     def log(self):
         """Logarithmic map for SE(2)
@@ -98,12 +100,14 @@ class SE2:
 
         This is the inverse operation to SE2.exp.
         """
-        return np.hstack([self.trans, SO2.log(self.rot)])
+        phi = SO2.log(self.rot)
+        rho = SO2.inv_left_jacobian(phi).dot(self.trans)
+        return np.hstack([rho, phi])
 
-    def asmatrix(self):
+    def as_matrix(self):
         """Return the 3x3 matrix representation of the
         transformation."""
-        R = self.rot.asmatrix()
+        R = self.rot.as_matrix()
         t = np.reshape(self.trans, (2, 1))
         return np.vstack([np.hstack([R, t]),
                           np.array([0, 0, 1])])
@@ -122,7 +126,7 @@ class SE2:
 
     def adjoint(self):
         """Return the adjoint matrix of the transformation."""
-        rotpart = self.rot.asmatrix()
+        rotpart = self.rot.as_matrix()
         transpart = np.array([self.trans[1], -self.trans[0]]).reshape((2, 1))
         return np.vstack([np.hstack([rotpart, transpart]),
                           [0, 0, 1]]
@@ -138,7 +142,7 @@ class SE2:
             return self.rot * other + self.trans
         else:
             # Transform one or more 3-vectors or fail
-            return self.asmatrix().dot(other)
+            return self.as_matrix().dot(other)
 
     def __repr__(self):
-        return "SE(2) Transformation Matrix \n %s" % self.asmatrix()
+        return "SE(2) Transformation Matrix \n %s" % self.as_matrix()

liegroups/se3.py
@@ -6,8 +6,8 @@ from liegroups.so3 import SO3
 class SE3:
     """Homogeneous transformation matrix in SE(3)
 
-    T = [[rot trans]
-         [  0     1]]
+    T = [[T.rot T.trans]
+         [    0       1]]
     """
 
     def __init__(self, rot=SO3.identity(), trans=np.zeros(3)):
@@ -23,19 +23,19 @@ class SE3:
         self.trans = trans
 
     @classmethod
-    def frommatrix(cls, mat):
+    def from_matrix(cls, mat):
         """Create a SE3 object from a 4x4 transformation matrix."""
-        if not SE3.isvalidmatrix(mat):
+        if not SE3.is_valid_matrix(mat):
             raise ValueError("Invalid transformation matrix")
 
         return cls(SO3(mat[0:3, 0:3]), mat[0:3, 3])
 
     @classmethod
-    def isvalidmatrix(cls, mat):
+    def is_valid_matrix(cls, mat):
         """Check if a matrix is a valid transformation matrix."""
         return mat.shape == (4, 4) and \
             np.array_equal(mat[3, :], np.array([0, 0, 0, 1])) and \
-            SO3.isvalidmatrix(mat[0:3, 0:3])
+            SO3.is_valid_matrix(mat[0:3, 0:3])
 
     @classmethod
     def identity(cls):
@@ -75,16 +75,14 @@ class SE3:
         Computes a transformation matrix from SE(3)
         tangent vector.
 
-        This isn't quite right because the translational
-        component should be multiplied by the inverse SO(3)
-        Jacobian, but we don't really need this.
-
         This is the inverse operation to SE3.log.
         """
         if xi.size != 6:
             raise ValueError("xi must have size 6")
 
-        return cls(SO3.exp(xi[3:6]), xi[0:3])
+        rho = xi[0:3]
+        phi = xi[3:6]
+        return cls(SO3.exp(phi), SO3.left_jacobian(phi).dot(rho))
 
     def log(self):
         """Logarithmic map for SE(3)
@@ -92,18 +90,16 @@ class SE3:
         Computes a SE(3) tangent vector from a transformation
         matrix.
 
-        This isn't quite right because the translational
-        component should be multiplied by the inverse SO(3)
-        Jacobian, but we don't really need this.
-
         This is the inverse operation to SE3.exp.
         """
-        return np.hstack([self.trans, SO3.log(self.rot)])
+        phi = SO3.log(self.rot)
+        rho = SO3.inv_left_jacobian(phi).dot(self.trans)
+        return np.hstack([rho, phi])
 
-    def asmatrix(self):
+    def as_matrix(self):
         """Return the 4x4 matrix representation of the
         transformation."""
-        R = self.rot.asmatrix()
+        R = self.rot.as_matrix()
         t = np.reshape(self.trans, (3, 1))
         return np.vstack([np.hstack([R, t]),
                           np.array([0, 0, 0, 1])])
@@ -122,7 +118,7 @@ class SE3:
 
     def adjoint(self):
         """Return the adjoint matrix of the transformation."""
-        rotmat = self.rot.asmatrix()
+        rotmat = self.rot.as_matrix()
         return np.vstack(
             [np.hstack([rotmat,
                         SO3.wedge(self.trans).dot(rotmat)]),
@@ -139,7 +135,7 @@ class SE3:
             return self.rot * other + self.trans
         else:
             # Transform one or more 4-vectors or fail
-            return self.asmatrix().dot(other)
+            return self.as_matrix().dot(other)
 
     def __repr__(self):
-        return "SE(3) Transformation Matrix \n %s" % self.asmatrix()
+        return "SE(3) Transformation Matrix \n %s" % self.as_matrix()

liegroups/so2.py
@@ -7,21 +7,21 @@ class SO2:
 
     def __init__(self, mat=np.identity(2)):
         """Create a SO2 object from a 2x2 rotation matrix."""
-        if not SO2.isvalidmatrix(mat):
+        if not SO2.is_valid_matrix(mat):
             raise ValueError("Invalid rotation matrix")
 
         self.mat = mat
 
     @classmethod
-    def frommatrix(cls, mat):
+    def from_matrix(cls, mat):
         """Create a SO2 object from a 2x2 rotation matrix."""
-        if not SO2.isvalidmatrix(mat):
+        if not SO2.is_valid_matrix(mat):
             raise ValueError("Invalid rotation matrix")
 
         return cls(mat)
 
     @classmethod
-    def isvalidmatrix(cls, mat):
+    def is_valid_matrix(cls, mat):
         """Check if a matrix is a valid rotation matrix."""
         return mat.shape == (2, 2) and np.isclose(np.linalg.det(mat), 1.) and \
             np.allclose(mat.T.dot(mat), np.identity(2))
@@ -61,6 +61,34 @@ class SO2:
         return Phi[1, 0]
 
     @classmethod
+    def left_jacobian(cls, phi):
+        """Left SO(2) Jacobian (see Barfoot/Eade).
+        """
+        # Near angle is close to 0, use first order Taylor expansion
+        # TODO: 1st order term
+        if np.isclose(phi, 0.):
+            return np.identity(2)
+
+        s = np.sin(phi)
+        c = np.cos(phi)
+
+        return (1. / phi) * np.array([[s, -(1 - c)],
+                                      [1 - c, s]])
+
+    @classmethod
+    def inv_left_jacobian(cls, phi):
+        """Inverse left SO(2) Jacobian (see Barfoot/Eade).
+        """
+        # Near angle is close to 0, use first order Taylor expansion
+        # TODO: 1st order term
+        if np.isclose(phi, 0.):
+            return np.identity(2)
+
+        A = np.sin(phi) / phi
+        B = (1. - np.cos(phi)) / phi
+        return (1. / (A * A + B * B)) * np.array([[A, B], [-B, A]])
+
+    @classmethod
     def exp(cls, phi):
         """Exponential map for SO(2).
 
@@ -79,7 +107,7 @@ class SO2:
         """
         return np.arctan2(self.mat[1, 0], self.mat[0, 0])
 
-    def asmatrix(self):
+    def as_matrix(self):
         """Return the 2x2 matrix representation of the rotation."""
         return self.mat
 
@@ -111,4 +139,4 @@ class SO2:
             return np.dot(self.mat, other)
 
     def __repr__(self):
-        return "SO(2) Rotation Matrix \n %s" % self.asmatrix()
+        return "SO(2) Rotation Matrix \n %s" % self.as_matrix()

liegroups/so3.py
@@ -7,21 +7,21 @@ class SO3:
 
     def __init__(self, mat=np.identity(3)):
         """Create a SO3 object from a 3x3 rotation matrix."""
-        if not SO3.isvalidmatrix(mat):
+        if not SO3.is_valid_matrix(mat):
             raise ValueError("Invalid rotation matrix")
 
         self.mat = mat
 
     @classmethod
-    def frommatrix(cls, mat):
+    def from_matrix(cls, mat):
         """Create a SO3 object from a 3x3 rotation matrix."""
-        if not SO3.isvalidmatrix(mat):
+        if not SO3.is_valid_matrix(mat):
             raise ValueError("Invalid rotation matrix")
 
         return cls(mat)
 
     @classmethod
-    def isvalidmatrix(cls, mat):
+    def is_valid_matrix(cls, mat):
         """Check if a matrix is a valid rotation matrix."""
         return mat.shape == (3, 3) and np.isclose(np.linalg.det(mat), 1.) and \
             np.allclose(mat.T.dot(mat), np.identity(3))
@@ -91,6 +91,50 @@ class SO3:
         return np.array([Phi[2, 1], Phi[0, 2], Phi[1, 0]])
 
     @classmethod
+    def left_jacobian(cls, phi):
+        """Left SO(3) Jacobian (see Barfoot).
+        """
+        if phi.size != 3:
+            raise ValueError("phi must have size 3")
+
+        angle = np.linalg.norm(phi)
+
+        # Near angle is close to 0, use first order Taylor expansion
+        # TODO: 1st order term
+        if np.isclose(angle, 0.):
+            return np.identity(3)
+
+        axis = phi / angle
+        s = np.sin(angle)
+        c = np.cos(angle)
+
+        return (s / angle) * np.identity(3) + \
+            (1 - s / angle) * np.outer(axis, axis) + \
+            ((1 - c) / angle) * SO3.wedge(axis)
+
+    @classmethod
+    def inv_left_jacobian(cls, phi):
+        """Inverse left SO(3) Jacobian (see Barfoot).
+        """
+        if phi.size != 3:
+            raise ValueError("phi must have size 3")
+
+        angle = np.linalg.norm(phi)
+
+        # Near angle is close to 0, use first order Taylor expansion
+        # TODO: 1st order term
+        if np.isclose(angle, 0.):
+            return np.identity(3)
+
+        axis = phi / angle
+        half_angle = 0.5 * angle
+        cot_half_angle = 1. / np.tan(half_angle)
+
+        return half_angle * cot_half_angle * np.identity(3) + \
+            (1 - half_angle * cot_half_angle) * np.outer(axis, axis) - \
+            half_angle * SO3.wedge(axis)
+
+    @classmethod
     def exp(cls, phi):
         """Exponential map for SO(3).
 
@@ -105,7 +149,7 @@ class SO3:
 
         # Near angle is close to 0, use first order Taylor expansion
         if np.isclose(angle, 0.):
-            return np.identity(3) + cls.wedge(phi)
+            return cls(np.identity(3) + cls.wedge(phi))
 
         axis = phi / angle
         s = np.sin(angle)
@@ -143,7 +187,7 @@ class SO3:
         # Otherwise normalize the axis and return the axis-angle vector
         return 0.5 * angle * axis / sin_angle
 
-    def asmatrix(self):
+    def as_matrix(self):
         """Return the 3x3 matrix representation of the rotation."""
         return self.mat
 
@@ -175,4 +219,4 @@ class SO3:
             return np.dot(self.mat, other)
 
     def __repr__(self):
-        return "SO(3) Rotation Matrix \n %s" % self.asmatrix()
+        return "SO(3) Rotation Matrix \n %s" % self.as_matrix()

tests/test_se2.py
@@ -8,7 +8,7 @@ def test_mul():
                   [1, 0, 0.5],
                   [0, 0, 1]])
     T2 = T.dot(T)
-    assert np.allclose((SE2.frommatrix(T) * SE2.frommatrix(T)).asmatrix(), T2)
+    assert np.allclose((SE2.from_matrix(T) * SE2.from_matrix(T)).as_matrix(), T2)
 
 
 def test_wedge_vee():
@@ -19,19 +19,19 @@ def test_wedge_vee():
 
 def test_exp_log():
     T = SE2.exp(np.array([1, 2, 3]))
-    assert np.allclose(SE2.exp(SE2.log(T)).asmatrix(), T.asmatrix())
+    assert np.allclose(SE2.exp(SE2.log(T)).as_matrix(), T.as_matrix())
 
 
 def test_normalize():
     T = SE2.exp(np.array([1, 2, 3]))
     T.rot.mat += 0.1
     T.normalize()
-    assert SE2.isvalidmatrix(T.asmatrix())
+    assert SE2.is_valid_matrix(T.as_matrix())
 
 
 def test_inverse():
     T = SE2.exp(np.array([1, 2, 3]))
-    assert np.allclose((T * T.inverse()).asmatrix(), np.identity(3))
+    assert np.allclose((T * T.inverse()).as_matrix(), np.identity(3))
 
 
 def test_adjoint():

tests/test_se3.py
@@ -9,7 +9,7 @@ def test_mul():
                   [1, 0, 0, -0.5],
                   [0, 0, 0, 1]])
     T2 = T.dot(T)
-    assert np.allclose((SE3.frommatrix(T) * SE3.frommatrix(T)).asmatrix(), T2)
+    assert np.allclose((SE3.from_matrix(T) * SE3.from_matrix(T)).as_matrix(), T2)
 
 
 def test_wedge_vee():
@@ -20,19 +20,19 @@ def test_wedge_vee():
 
 def test_exp_log():
     T = SE3.exp(np.array([1, 2, 3, 4, 5, 6]))
-    assert np.allclose(SE3.exp(SE3.log(T)).asmatrix(), T.asmatrix())
+    assert np.allclose(SE3.exp(SE3.log(T)).as_matrix(), T.as_matrix())
 
 
 def test_normalize():
     T = SE3.exp(np.array([1, 2, 3, 4, 5, 6]))
     T.rot.mat += 0.1
     T.normalize()
-    assert SE3.isvalidmatrix(T.asmatrix())
+    assert SE3.is_valid_matrix(T.as_matrix())
 
 
 def test_inverse():
     T = SE3.exp(np.array([1, 2, 3, 4, 5, 6]))
-    assert np.allclose((T * T.inverse()).asmatrix(), np.identity(4))
+    assert np.allclose((T * T.inverse()).as_matrix(), np.identity(4))
 
 
 def test_adjoint():

tests/test_so2.py
@@ -27,11 +27,16 @@ def test_exp_log():
     assert np.allclose(SO2.exp(SO2.log(C)).mat, C.mat)
 
 
+def test_exp_log_zeros():
+    C = SO2.exp(0)
+    assert np.allclose(SO2.exp(SO2.log(C)).mat, C.mat)
+
+
 def test_normalize():
     C = SO2.exp(np.pi / 4)
     C.mat += 0.1
     C.normalize()
-    assert SO2.isvalidmatrix(C.mat)
+    assert SO2.is_valid_matrix(C.mat)
 
 
 def test_inverse():

tests/test_so3.py
@@ -61,11 +61,16 @@ def test_exp_log():
     assert np.allclose(SO3.exp(SO3.log(C)).mat, C.mat)
 
 
+def test_exp_log_zeros():
+    C = SO3.exp(np.zeros(3))
+    assert np.allclose(SO3.exp(SO3.log(C)).mat, C.mat)
+
+
 def test_normalize():
     C = SO3.exp(np.pi * np.ones(3) / 4)
     C.mat += 0.1
     C.normalize()
-    assert SO3.isvalidmatrix(C.mat)
+    assert SO3.is_valid_matrix(C.mat)
 
 
 def test_inverse():