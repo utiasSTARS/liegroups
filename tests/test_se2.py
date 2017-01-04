173696c26cf1bbdafc23e448ace710825b4c306f
Lee Clement <clement.leopold@gmail.com>
2017-01-03 22:19:12 -0500

    Got rid of awkward bindto methods. Shallow copy via copy.copy does the same thing.

--- tests/test_se2.py
+++ tests/test_se2.py
@@ -5,13 +5,6 @@ import numpy as np
 from liegroups import SE2
 
 
-def test_bindto():
-    T1 = SE2.identity()
-    T2 = SE2.identity()
-    T2.bindto(T1)
-    assert(T1 is not T2 and T1.rot is T2.rot and T1.trans is T2.trans)
-
-
 def test_identity():
     T = SE2.identity()
     assert isinstance(T, SE2)
