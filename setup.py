e1d5297f7c21942084f869d4483634b37dfb5fd7
Lee Clement <clement.leopold@gmail.com>
2017-02-21 17:54:05 -0500

    Update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='liegroups',
-    version='0.0.0',
+    version='0.1.0',
     description='Lie groups in Python',
     author='Lee Clement',
     author_email='lee.clement@robotics.utias.utoronto.ca',
